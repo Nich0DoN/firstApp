620690a5f42459e04d63ef5e0c855a5757c2a979
Nich0DoN <nicholasdonahue@mail.ru>
2023-03-14 21:08:50 +0300

    Add registration Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Обработать успешное сохранение.
+      flash[:success] = "Welcome to the Website!"
+      redirect_to @user
+      # redirect_touser_url(@user)
+    else
+      render 'new'
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
